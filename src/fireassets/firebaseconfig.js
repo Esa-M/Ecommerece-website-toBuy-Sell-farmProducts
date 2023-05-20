9a808c66751c08929be499295ecc6aa7c17eb8e7
Esa-M <esaofficial.023@gmail.com>
2023-05-19 18:19:22 -0600

    "first commit"

--- src/fireassets/firebaseconfig.js
+++ src/fireassets/firebaseconfig.js
@@ -4,13 +4,13 @@ import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1z4Gs0zPcBYSUjfA20f37zcii-qDKkYs",
-  authDomain: "aqua-bf4fc.firebaseapp.com",
-  projectId: "aqua-bf4fc",
-  storageBucket: "aqua-bf4fc.appspot.com",
-  messagingSenderId: "469533666604",
-  appId: "1:469533666604:web:44f13ff737c0af2860d089",
-  measurementId: "G-TCQRT70DNM"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
